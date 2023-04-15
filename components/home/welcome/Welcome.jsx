d24391c72514336951e245356acc9a5401ff7f6e
wnathanael <w.nathanael28@gmail.com>
2023-04-15 16:49:55 +0200

    implemented search bar

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { 
+  View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native';
+import { useRouter } from 'expo-router';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello William</Text>
+        <Text style={styles.welcomeMessage}>Find your dream job!</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder='What are you looking for?'
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      
+
     </View>
   )
 }
