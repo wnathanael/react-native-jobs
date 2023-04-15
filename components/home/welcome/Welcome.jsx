f6a90d2b9ebfadca34d260fbf2c1c11aca6c6e45
wnathanael <w.nathanael28@gmail.com>
2023-04-15 17:00:09 +0200

    implemented welcome component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,10 +12,12 @@ import { useRouter } from 'expo-router';
 import styles from './welcome.style';
 import { icons, SIZES } from '../../../constants';
 
-const jobTypes = ["Full-time", "Part-time", "Contractor"];
+const jobTypes = ["Full-time", "Part-time", "Contractor", "a", "b", "c", "d", "e", "f"];
 
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time")
+
   return (
     <View>
       <View style={styles.container}>
@@ -41,7 +43,25 @@ const Welcome = () => {
         </TouchableOpacity>
       </View>
 
-      
+      <View style={styles.tabsContainer}>
+        <FlatList 
+          data={jobTypes}
+          renderItem={({item}) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+        />
+      </View>
 
     </View>
   )
