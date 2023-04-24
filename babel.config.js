6ff57d9da9f10728aa70968db5e3f2096af06059
wnathanael <w.nathanael28@gmail.com>
2023-04-24 20:41:28 +0200

    fixed hook to env

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,32 @@
-
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ["module:react-native-dotenv", {
-      "envName": "APP_ENV",
-      "moduleName": "@env",
-      "path": ".env",
-      "safe": false,
-      "allowUndefined": true,
-      "verbose": false
-    }]
-  ]
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+      require.resolve("expo-router/babel"),
+      ["module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+        "safe": false,
+        "allowUndefined": true,
+        "verbose": false
+      }]
+    ],
+  };
 };
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: [
+//     ["module:react-native-dotenv", {
+//       "envName": "APP_ENV",
+//       "moduleName": "@env",
+//       "path": ".env",
+//       "safe": false,
+//       "allowUndefined": true,
+//       "verbose": false
+//     }]
+//   ]
+// };
